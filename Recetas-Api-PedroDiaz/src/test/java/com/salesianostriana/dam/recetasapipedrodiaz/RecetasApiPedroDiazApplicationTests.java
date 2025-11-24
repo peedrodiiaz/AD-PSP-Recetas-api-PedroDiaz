bdf1aeade481a26c6a5f783561bb88beb8821be7
Pedro <diazpd2006>
2025-11-24 19:13:21 +0100

    Configuración inicial

---
+++ Recetas-Api-PedroDiaz/src/test/java/com/salesianostriana/dam/recetasapipedrodiaz/RecetasApiPedroDiazApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.recetasapipedrodiaz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecetasApiPedroDiazApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
